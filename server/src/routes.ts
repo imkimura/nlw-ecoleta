4a16f75bb929be38a78b7f31ca0ec9aaa22781ca
imkimura <juliakimura@univem.edu.br>
2020-06-24 12:19:05 -0300

    finish 2 day of next level week

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,17 @@
-import express from 'express';
+import express, { response } from 'express';
+import knex from './database/connection';
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/users', (req, res) => {
-    console.log('Hello People');
+routes.get('/items', itemsController.index);
 
-    res.json({'result':'Hello World!'});
-});
+routes.get('/points', pointsController.index);
+routes.post('/points', pointsController.create);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
